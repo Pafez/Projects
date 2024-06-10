c9dbc9e0d926d58a6ff1a4e0c953446909b5ad04
Pafez <shpafez@gmail.com>
2024-06-10 20:34:52 +0600

    modified:   snake/main.c

--- snake/main.c
+++ snake/main.c
@@ -2,11 +2,35 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 400
 
+#define CELL_WIDTH 10
+#define CELL_HEIGHT 10
 
 int main() {
+    int run = 1;
 
-    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("DVD_LOGO", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_Rect snake_body = {};
+
+    while (run) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                run = 0;
+                break;
+            }
+        }
+        if (!run) break;
+
+        
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
+    }
 
     return 0;
 }
